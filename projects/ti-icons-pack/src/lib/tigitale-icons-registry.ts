6af9d8f52ca40c742dc925ccb433f77c6b83f52c
Bruno Silva <brunosilv_a@outlook.com>
2020-11-23 18:46:51 -0300

    correçao name

--- projects/ti-icons-pack/src/lib/tigitale-icons-registry.ts
+++ projects/ti-icons-pack/src/lib/tigitale-icons-registry.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-// import { tigitaleIcon, TigitaleIcon } from './tigitale-icons';
+import { tigitaleIcon, TigitaleIcon } from './tigitale-icons';
 import {
-  tigitaleIcon,
-  TigitaleIcon,
   tigitaleIcons360,
   tigitaleIconsAdvertisement,
   tigitaleIconsAirplay,
